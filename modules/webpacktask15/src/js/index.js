9360bfaa8e49f733d28ef21aa74a7f55d01cc95e
Wen <wfwuestc@163.com>
2017-09-19 16:11:44 +0800

    模块导入方法分类

--- modules/webpacktask15/src/js/index.js
+++ modules/webpacktask15/src/js/index.js
@@ -1,14 +1,28 @@
-import '../css/main.css'
-import '../css/foot.css'
-import '../css/header.css'
+// 方法1 使用import导入
+// import '../css/main.css'
+// import '../css/foot.css'
+// import '../css/header.css'
+//
+// import $ from 'jquery'
+// import GoTop from './gotop'
+// import Barrel from './waterfall'
+// import Carousel from './carousel'
 
-import $ from 'jquery'
-import GoTop from './gotop'
-import Barrel from './waterfall'
-import Carousel from './carousel'
+require('../css/main.css')
+require('../css/foot.css')
+require('../css/header.css')
+//方法2 使用require
+// var Carousel = require('./carousel')
+// var Waterfall = require('./waterfall')
+// var GoTop = require('./gotop')
+//
+// Carousel()
+// Waterfall()
+// GoTop()
 
-
-
-Carousel()
-Waterfall()
-GoTop()
+// 方法3 使用AMD define
+define(['./carousel', './gotop', './waterfall'],function (slideshow, goTop, waterFall1) {
+  slideshow()
+  goTop()
+  waterFall1()
+})
